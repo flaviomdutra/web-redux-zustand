feb0b93b234caf1ddab601a373e40ea06cf885b1
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-24 11:36:12 -0300

    Fix null checks and update dependencies

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -22,7 +22,7 @@ export function Module({
     (state) => state.player,
   )
   const lessons = useAppSelector(
-    (state) => state.player.course.modules[moduleIndex].lessons,
+    (state) => state.player.course?.modules[moduleIndex].lessons,
   )
 
   return (
@@ -41,7 +41,7 @@ export function Module({
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
+          {lessons?.map((lesson, lessonIndex) => {
             const isCurrent =
               currentModuleIndex === moduleIndex &&
               currentLessonIndex === lessonIndex
